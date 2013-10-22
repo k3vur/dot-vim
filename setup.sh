d0595aea2fde3b17f52f293b1d91c35843099acd
kevin.urban <theonlyulti@gmail.com>
2013-10-22 19:01:08 +0200

    don't abort anymore if macvim is not installed but recommend using it (when on mac)

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
-if ! which -s mvim; then
-	echo "error: mvim not found"
-	echo "install MacVim and try again"
-	exit 1
-fi
-
 if ! which -s git; then
 	echo "error: git not found"
 	echo "install git and try again"
@@ -19,7 +13,7 @@ git submodule update
 
 echo
 echo "installing vundle bundles"
-mvim -v +BundleInstall +qall
+vim +BundleInstall +qall
 
 echo
 echo "compiling YouCompleteMe..."
@@ -30,4 +24,15 @@ popd > /dev/null
 echo
 echo "symlinking .vimrc"
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
+
+if [ $(uname -s) = "Darwin" ]; then
+	echo
+	if ! which -s mvim; then
+		echo "consider installing MacVim (old vim versions don't support all the plugins):"
+		echo "brew install macvim"
+		echo
+	fi
+	echo "also consider symlinking vim to mvim (given that $HOME/bin is in your \$PATH):"
+	echo "ln -s $(which mvim) $HOME/bin/vim"
+fi
 
